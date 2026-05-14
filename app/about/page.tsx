59a303a9c34b10759cdb00d60243a5157c25e781
bellahowell <isabellahowell16@gmail.com>
2026-05-13 18:47:39 -0700

    initial commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main style={{ padding: 60 }}>
       <h1>Hi, I’m Isabella 👋</h1>
-      <p>Welcome to my Next.js portfolio.</p>
+
+      <nav style={{ marginTop: 20 }}>
+        <Link href="/about">Go to About</Link>
+      </nav>
     </main>
   );
 }
